eb30d928ba596d82c0938027155c6d4a02c823f0
Kelsey Josund <kelsey2@stanford.edu>
2015-02-19 15:54:55 -0800

    first commit

---
+++ translate.py
@@ -0,0 +1,58 @@
+
+class Translator:
+
+	def __init__(self):
+		self.dictionary = dict()
+		self.sentences = list()
+		self.translation = list()
+
+	# starter function to look up a word (later make more complicated/intelligent)
+	def translate(self, word):
+		options = self.dictionary[word.lower()]
+		return options[0] # this can be modified later to not just use the first translation option
+
+	# basic direct translation
+	def stupid_translate(self):
+		for line in self.sentences:
+			english_sentence = list()
+			for word in line:
+				english_word = self.translate(word)
+				print english_word
+				english_sentence.append(english_word)
+				self.translation.append(english_sentence)
+
+	# read in a file and convert into usable form
+	def read_file(self, file_name):
+		contents = dict()
+		f = open(file_name)
+		for line in f:
+			contents.append(line)
+		f.close()
+		full = '\n'.join(contents)
+		result = full.split()
+		return result
+
+	# reads in our mini dictionary and makes usable
+	def parse_dict(self, dictionary):
+		print "HERE"
+		f = open(dictionary)
+		for line in f:
+			split = f.split(':')
+			print split
+			if len(split) == 2:
+				key = split[0] # spanish word
+				values = split[1].split(',') # multiple possible english words for a single spanish word
+				self.dictionary[key] = values
+			else:
+				print split # if anything prints, there's a problem with the dictionary
+
+def main():
+	sentences_file = '../data/sentences.txt'
+	dictionary_file = '../data/dictionary.txt'
+	t = Translator()
+	t.parse_dict(dictionary_file)
+	t.sentences = t.read_file(sentences_file)
+	t.stupid_translate()
+
+if __name__ == '__main':
+	main()
